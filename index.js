b6b7ceeb528c2c497313d35d1df60ac107ed00a3
unknown <197130@ppu.edu.ps>
2022-07-20 13:32:01 +0300

    after moify index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 console.log("index.js is run0");
 let stackValue=[];
-// let innerHtmlV=`<div></div>`;
-let innerHtmlV = document.createElement("div");
+let helpStak=[];
+let innerHtmlV=`<div></div>`;
+// let innerHtmlV = document.createElement("div");
 
 
 function push(){
@@ -11,18 +12,22 @@ if(number==""){
     console.log("the value is null")
     alert("the value is null");
 }else{
-    innerHtmlV.innerText=`${number}`;
-    let str =document.getElementById("first-child");
-/    stackValue.push(number);
+    // innerHtmlV.innerText=`${number}`;
+    // let str =document.getElementById("first-child");
+    stackValue.push(number);
     console.log(number);
 
-//     for(let val of stackValue){
-//         console.log(val);
-//         innerHtmlV +=`<div>${val}</div>`;
+    for(let val of stackValue){
+        console.log(val);
 
-//     console.log(innerHtmlV);
+        innerHtmlV +=`<div>${val}</div>`;
+
+    console.log(innerHtmlV);
+    helpStak.push(number);
+    stackValue=[];
+    console.log(helpStak);
     
-// }
+}
 document.getElementById("stack").innerHTML = innerHtmlV;
 }
 
@@ -32,7 +37,20 @@ document.getElementById("stack").innerHTML = innerHtmlV;
 }
 
 function pop(){
-    stackValue.pop();
+    console.log("pop is run");
+    helpStak.pop();
+    console.log(helpStak)
+    for(let val of helpStak){
+        console.log(val);
 
+        innerHtmlV +=`<div>${val}</div>`;
+
+    console.log(innerHtmlV);
+    // stackValue=[];
+    
+}
+document.getElementById("stack").innerHTML = '<div></div>';
+
+document.getElementById("stack").innerHTML = innerHtmlV;
     
 }
